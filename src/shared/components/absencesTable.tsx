04a493966824e4263430711616abe579329cba42
HarryGu <harry.guapi@gmail.com>
2023-02-02 12:32:03 -0600

    add table filter, paginator and styles

--- src/shared/components/absencesTable.tsx
+++ src/shared/components/absencesTable.tsx
@@ -1,6 +1,10 @@
 import { DataTable } from "primereact/datatable"
 import { Column } from "primereact/column";
-import "primereact/resources/themes/lara-light-blue/theme.css"
+import "./DataTableDemo.css"
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.css';
+import 'primeflex/primeflex.css';
 import { FilterMatchMode, FilterOperator } from 'primereact/api';
 import { useEffect, useState } from "react";
 import { EmployeeState } from "shared/model/employeeState";
@@ -12,7 +16,13 @@ import { AbsenceDto } from "shared/model/absenceDTO";
 export const AbsencesTable = (props: EmployeeState) => {
     const [membersData, setMembersData] = useState<Member[]>([])
     const [absencesData, setAbsencesData] = useState<Absence[]>([])
-    const [displayData, setdisplayData] = useState<AbsenceDto[]>([])       
+    const [displayData, setdisplayData] = useState<AbsenceDto[]>([])
+    const [loading, setLoading] = useState(true);    
+    const [filters, setFilters] = useState({
+        'type': { value: null, matchMode: FilterMatchMode.CONTAINS },
+        'startDate': { value: null, matchMode: FilterMatchMode.CONTAINS },
+        'endDate': { value: null, matchMode: FilterMatchMode.CONTAINS }
+    });
     
     useEffect(() => {
         setMembersData(props.members);
@@ -23,20 +33,42 @@ export const AbsencesTable = (props: EmployeeState) => {
             const newStatus: string = e.rejectedAt ? 'Rejected' : e.confirmedAt? 'Confirmed' : 'Requested'
             const newAbsenceDto: AbsenceDto = {...e, name: absencesMember?.name, status: newStatus}
             setdisplayData((oldDisplayArray : AbsenceDto[])=>[...oldDisplayArray, newAbsenceDto])
+            setLoading(false)
         })
+        return ()=>{
+            setLoading(true)
+        }
     }, [absencesData, membersData, props])
-    
+
+    const statusBodyTemplate = (rowData:AbsenceDto) => {
+        return <span className={`customer-badge status-${rowData.status}`}>{rowData.status}</span>;
+    }
+
     return (
-        <div>
-            <div>
-                <DataTable value={displayData} responsiveLayout="scroll">
-                    <Column field="name" header="Name"></Column>
-                    <Column field="type" header="Type"></Column>
-                    <Column field="startDate" header="StartDate"></Column>
-                    <Column field="endDate" header="EndDate"></Column>
-                    <Column field="memberNote" header="MemberNote"></Column>
-                    <Column field="status" header="Status"></Column>
-                    <Column field="admitterNote" header="AdmitterNote"></Column>
+        <div className="datatable-filter-demo">
+            <div className="card">
+                <DataTable value={displayData} paginator 
+                className="p-datatable-employee" showGridlines rows={10} 
+                filters={filters}
+                filterDisplay="row"
+                loading={loading}
+                emptyMessage="No employees found."
+                responsiveLayout="scroll">
+                    <Column field="name" header="Name"
+                    style={{ width: '6vw' }} 
+                    ></Column>
+                    <Column field="type" header="Type"  
+                    style={{ width: '12vw' }} 
+                    filter filterPlaceholder="type name"></Column>
+                    <Column field="startDate" header="StartDate"
+                    style={{ width: '14vw' }} 
+                     filter filterPlaceholder="start date"></Column>
+                    <Column field="endDate" header="EndDate"
+                    style={{ width: '14vw' }} 
+                     filter filterPlaceholder="end date"></Column>
+                    <Column field="memberNote" header="MemberNote" style={{ width: '25vw' }}></Column>
+                    <Column field="status" header="Status" style={{ width: '4vw' }}  body={statusBodyTemplate}></Column>
+                    <Column field="admitterNote" style={{ width: '25vw' }} header="AdmitterNote"></Column>
                 </DataTable>
             </div>
         </div>
